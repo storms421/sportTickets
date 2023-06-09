046854bc1d2b4b2a33e280cb52f0252120fae7a7
Noah Storms <noahstorms@yahoo.com>
2023-06-09 14:52:53 -0600

    Select Team Update
    Just adding some comments cause I forgot to add them earlier.

--- CustomerAccess.py
+++ CustomerAccess.py
@@ -1,27 +1,20 @@
 import DenverNuggetsTickets
 
 
+# This function is used to select teams for purchasing tickets
 def select_team(teams_list):
+    # While user is selecting, loop through teams
     while True:
+        # Lists all Colorado teams in array
         print("Check out our outstanding Colorado teams below!")
         # Loops through list of teams to present to customer
         for index in range(len(teams_list)):
             print("(" + str(index + 1) + ") " + teams_list[index])
 
+        # Looks for customers selection and returns when matched
         team_selection_number = input(
             "\nWhich sports team are we looking to purchase tickets from? (Enter the number): ")
         if team_selection_number.isdigit() and 1 <= int(team_selection_number) <= len(teams_list):
-            return teams_list[int(team_selection_number) - 1]
+            return teams_list[int(team_selection_number) - 1]  # Makes sure to return team in correct index
         else:
             print("Invalid input. Please try again.\n")
-
-
-  #  team_selection_number = input("\nWhich sports team are we looking to purchase tickets from?: ")  # User input
-
-  #  team_selection_number = int(team_selection_number)
-  #  if team_selection_number == 1:
-  #      print("Broncos")
-   # elif team_selection_number == 2:
-   #     DenverNuggetsTickets.denver_nuggets_tickets(team_seats, team_prices, teams_list[team_selection_number - 1])
-   # else:
-     #   print("Invalid Option!")
